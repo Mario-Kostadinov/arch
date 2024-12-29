cdf97c8a54637d8ee7f7236b3a49a6f6a326e5d5
Mario Kostdinov <mmkostadinov@gmail.com>
2024-12-29 17:16:50 +0200

    sway config changes

--- desktop/wayland/compositors/sway/setup_sway.sh
+++ desktop/wayland/compositors/sway/setup_sway.sh
@@ -4,6 +4,14 @@ sway_packages_install() {
    install_package "sway" 
 }
 
+sway_links() {
+    local config=~/projects/arch/desktop/wayland/compositors/sway/sway_config
+    local sway_config=~/.config/sway/config
+
+    ln -s $config $sway_config
+}
+
 setup_sway() {
    sway_packages_install 
+   sway_links
 }
--- install_desktop.sh
+++ install_desktop.sh
@@ -5,6 +5,7 @@ source ./utility/install_package_aur.sh
 source ./utility/log.sh
 
 source ./desktop/sound/setup_sound.sh
+source ./desktop/wayland/compositors/sway/setup_sway.sh
 
 logs() {
     delete_log_file
@@ -12,19 +13,27 @@ logs() {
 }
 
 sound() {
-    log_to_screen "---Sound setup---"
+    log_to_screen "Sound setup"
     setup_sound
-    log_to_screen "---Sound setup end---"
+    log_to_screen "Sound setup end"
+}
+
+display_manager() {
+    log_to_screen "Display Manager setup"
+    setup_sway
+    log_to_screen "Display Manager setup end"
 }
 
 setup_all() {
     sound
+    display_manager
 }
 
 main() {
  logs
  log_to_screen "---Starting Desktop Install---"
- #setup_all 
+ #setup_all
+ display_manager
  log_to_screen "Installation finished"
 }
 
